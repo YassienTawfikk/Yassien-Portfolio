ada5f3c1c284ae0f815413238089213c2f9c0cb6
Yassien Tawfik <yassien.m.m.tawfik@gmail.com>
2025-08-15 01:02:46 +0300

    enhance code for vercel deployment

--- api/index.py
+++ api/index.py
@@ -1,11 +1,1 @@
 from app import server as app
-
-
-def handler(event, context):
-    from werkzeug.wrappers import Request, Response
-
-    @Request.application
-    def application(request):
-        return app(request.environ, lambda status, headers: None)
-
-    return Response("OK")
