f420493241d37542d33bf92a6b63d30fc47aeca6
Yassien Tawfik <yassien.m.m.tawfik@gmail.com>
2025-12-12 01:06:14 +0200

    add educational gpa

--- pages/_04_education.py
+++ pages/_04_education.py
@@ -5,7 +5,7 @@ from utils.json_utils import get_json_values
 CONFIG_FILE_PATH = "data/_04_education.json"
 CSS_FILE_PATH = "../static/css/_04_education.css"
 
-image01, image02, university, faculty, degree, expected_graduation, description, biomedical_courses, technical_courses, mathematics_courses = \
+image01, image02, university, faculty, degree, expected_graduation, gpa, description, biomedical_courses, technical_courses, mathematics_courses = \
     (
         get_json_values(CONFIG_FILE_PATH, [
             ("image_01",),
@@ -14,6 +14,7 @@ image01, image02, university, faculty, degree, expected_graduation, description,
             ("faculty",),
             ("degree",),
             ("expected_graduation",),
+            ("gpa",),
             ("description",),
             ("key_courses", "biomedical_engineering"),
             ("key_courses", "ai_and_cs"),
@@ -31,7 +32,7 @@ layout = html.Div([
     html.Div(className='education-container', children=[
         html.Span('Education', className='head-font title'),
         html.Span(university + " - " + faculty, className='subtitle body-font'),
-        html.Span(degree + ' - Expected Graduation: ' + expected_graduation, className='subsubtitle body-font'),
+        html.Span(degree + ' - Expected Graduation: ' + expected_graduation + ' - GPA: ' + gpa, className='subsubtitle body-font'),
         html.Span(description, className='body-text body-font'),
         html.Div([
             html.Span('Key Courses', className='head-font'),
